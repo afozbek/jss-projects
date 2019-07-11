524e5b990600ea2589e649f31cf8107be8202e9b
afozbek <furkanozbek1995@gmail.com>
2019-07-11 17:10:59 +0300

    adding auth files

---
+++ FirstWebApp/src/servlets/training/auth/LogoutServlet.java
@@ -0,0 +1,20 @@
+package servlets.training.auth;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.invalidate();
+        resp.sendRedirect("/public/public.html");
+    }
+}
