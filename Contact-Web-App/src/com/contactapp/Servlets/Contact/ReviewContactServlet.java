81d33b39de66ca7df5e1456d489bb89df7a09c59
afozbek <furkanozbek1995@gmail.com>
2019-07-14 16:13:06 +0300

    Updating review contact servlet

--- Contact-Web-App/src/com/contactapp/Servlets/Contact/ReviewContactServlet.java
+++ Contact-Web-App/src/com/contactapp/Servlets/Contact/ReviewContactServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet("/contacts")
@@ -19,12 +18,8 @@ public class ReviewContactServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<ContactBean> contactList = ContactController.getContacts();
 
-        PrintWriter write = resp.getWriter();
-
-        for (ContactBean contact : contactList) {
-            write.println(contact.toString());
-        }
         req.setAttribute("contactList", contactList);
-//        req.getRequestDispatcher("/contact/contacts.jsp").forward(req, resp);
+
+        req.getRequestDispatcher("/contact/contacts.jsp").forward(req, resp);
     }
 }
