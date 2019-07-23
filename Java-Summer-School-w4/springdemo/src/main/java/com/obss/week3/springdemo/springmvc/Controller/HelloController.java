45ed5b381b7ed9eae42a0f14457a10a7691ebabb
afozbek <furkanozbek1995@gmail.com>
2019-07-23 19:49:39 +0300

    Updating controller

--- Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/springmvc/Controller/HelloController.java
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/springmvc/Controller/HelloController.java
@@ -2,7 +2,9 @@ package com.obss.week3.springdemo.springmvc.Controller;
 
 import com.obss.week3.springdemo.learningmanagementsystem.model.content.Content;
 import com.obss.week3.springdemo.learningmanagementsystem.model.content.ElearningContent;
-import com.obss.week3.springdemo.springmvc.Error.ErrorMessage;
+import com.obss.week3.springdemo.springmvc.Notifications.Messages.ErrorMessage;
+import com.obss.week3.springdemo.springmvc.Notifications.Messages.InfoMessage;
+import com.obss.week3.springdemo.springmvc.Notifications.Messages.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/")
 public class HelloController {
 
     private List<Content> contents = new ArrayList<>();
@@ -25,43 +27,55 @@ public class HelloController {
         contents.add(new ElearningContent(4L, "Silan", "https://silanerasmus.bilir", new Date(), new Date()));
     }
 
-    // GET http://localhost:8080/hello
     @GetMapping
+    @ResponseBody
+    public ResponseEntity<Message> getIndex() {
+        String message = "Welcome to Index Page 😊😊";
+        Message infoMessage = new InfoMessage(message);
+        return new ResponseEntity<Message>(infoMessage, HttpStatus.OK);
+    }
+
+    // GET http://localhost:8080/hello
+    @GetMapping("/hello")
     public List<Content> getContents() {
         return contents;
     }
 
     // GET http://localhost:8080/hello/1
-    @GetMapping("/{id}")
+    @GetMapping("/hello/{id}")
     public Content getContent(@PathVariable Long id) {
         return contents.stream().filter((content) -> id.equals(content.getId())).findFirst().orElse(null);
     }
 
     // GET http://localhost:8080/hello/search
-    @GetMapping("/search")
+    @GetMapping("/hello/search")
     public List<Content> findContents(@RequestParam String name) {
         return contents.stream().filter((content) -> content.getName()
                 .toLowerCase().contains(name)).collect(Collectors.toList());
     }
 
     // POST http://localhost:8080/hello/add-content
-    @PostMapping("/add-content")
+    @PostMapping("/hello/add-content")
     public ResponseEntity<?> addContent(@RequestBody ElearningContent contentObj) {
-        ErrorMessage errorMessage;
+        Message message;
+
         if (contentObj.getId() == null || contentObj.getName() == null) {
-            errorMessage = new ErrorMessage("Please fill required fields");
-            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+            message = new ErrorMessage("Please fill required fields");
+            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
         }
 
+        // null or Content obj
         Content existingContent = contents.stream()
                 .filter((content) -> contentObj.getId().equals(content.getId()))
-                .findFirst().orElse(null);
+                .findFirst()
+                .orElse(null);
 
+        // Content exist return error message
         if (existingContent != null) {
-            errorMessage = new ErrorMessage("Please Enter Different id");
-            return new ResponseEntity<>(errorMessage, HttpStatus.CONFLICT);
+            message = new ErrorMessage("Please Enter Different id");
+            return new ResponseEntity<>(message, HttpStatus.CONFLICT);
         }
-
+        
         contents.add(contentObj);
 
         return new ResponseEntity<>(contentObj, HttpStatus.OK);
---
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/springmvc/Notifications/Notify.java
@@ -0,0 +1,4 @@
+package com.obss.week3.springdemo.springmvc.Notifications;
+
+public class Notify {
+}
