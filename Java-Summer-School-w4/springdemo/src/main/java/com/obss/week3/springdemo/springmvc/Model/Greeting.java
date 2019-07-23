eebfb03cb8ccb89d09746331241f105a8faa8eba
afozbek <furkanozbek1995@gmail.com>
2019-07-23 16:39:24 +0300

    Adding Greeting Model

---
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/springmvc/Model/Greeting.java
@@ -0,0 +1,19 @@
+package com.obss.week3.springdemo.springmvc.Model;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
