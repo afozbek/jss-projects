aecd591e1432706a0a8d285a86d1d6362d752dcd
afozbek <furkanozbek1995@gmail.com>
2019-07-23 16:35:20 +0300

    Adding Config Files

--- Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/AppConfig.java
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/AppConfig.java
@@ -1,7 +1,9 @@
 package com.obss.week3.springdemo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 public class AppConfig {
 
--- Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/SpringdemoApplication.java
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/SpringdemoApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-
 @SpringBootApplication
 public class SpringdemoApplication {
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringdemoApplication.class, args);
         System.out.println("\nApplication is running correctly...");
