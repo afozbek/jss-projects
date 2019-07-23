ec91c736ab1c9c8e081752f2b66c7a8c201f2e7a
afozbek <furkanozbek1995@gmail.com>
2019-07-23 19:49:13 +0300

    Updating main classes

--- Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/SpringdemoApplication.java
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/SpringdemoApplication.java
@@ -1,9 +1,5 @@
 package com.obss.week3.springdemo;
 
-import com.obss.week3.springdemo.learningmanagementsystem.model.content.ElearningContent;
-import com.obss.week3.springdemo.learningmanagementsystem.model.content.VideoContent;
-import com.obss.week3.springdemo.learningmanagementsystem.service.impl.ElearningServiceImpl;
-import com.obss.week3.springdemo.learningmanagementsystem.service.impl.VideoServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,22 +10,5 @@ public class SpringdemoApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringdemoApplication.class, args);
         System.out.println("\nApplication is running correctly...");
-
-        ElearningContent elearningContent = context.getBean(ElearningContent.class);
-        VideoContent videoContent = context.getBean(VideoContent.class);
-
-        ElearningServiceImpl elearningService = context.getBean(ElearningServiceImpl.class);
-        VideoServiceImpl videoService = context.getBean(VideoServiceImpl.class);
-
-        elearningService.createNewElearningContent((long) 1, elearningContent);
-        elearningService.createNewElearningContent((long) 2, elearningContent);
-        elearningService.createNewElearningContent((long) 3, elearningContent);
-        elearningService.createNewElearningContent((long) 4, elearningContent);
-
-        videoService.createNewVideoContent((long) 5, videoContent);
-        videoService.createNewVideoContent((long) 6, videoContent);
-
-        elearningService.getContentCache().printCacheSize();
-        videoService.getContentCache().printCacheSize();
     }
 }
--- Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/learningmanagementsystem/ScopeApp.java
+++ Java-Summer-School-w4/springdemo/src/main/java/com/obss/week3/springdemo/learningmanagementsystem/ScopeApp.java
@@ -1,4 +1,35 @@
 package com.obss.week3.springdemo.learningmanagementsystem;
 
+import com.obss.week3.springdemo.SpringdemoApplication;
+import com.obss.week3.springdemo.learningmanagementsystem.model.content.ElearningContent;
+import com.obss.week3.springdemo.learningmanagementsystem.model.content.VideoContent;
+import com.obss.week3.springdemo.learningmanagementsystem.service.impl.ElearningServiceImpl;
+import com.obss.week3.springdemo.learningmanagementsystem.service.impl.VideoServiceImpl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class ScopeApp {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringdemoApplication.class, args);
+        System.out.println("\nApplication is running correctly...");
+
+        ElearningContent elearningContent = context.getBean(ElearningContent.class);
+        VideoContent videoContent = context.getBean(VideoContent.class);
+
+        ElearningServiceImpl elearningService = context.getBean(ElearningServiceImpl.class);
+        VideoServiceImpl videoService = context.getBean(VideoServiceImpl.class);
+
+        elearningService.createNewElearningContent((long) 1, elearningContent);
+        elearningService.createNewElearningContent((long) 2, elearningContent);
+        elearningService.createNewElearningContent((long) 3, elearningContent);
+        elearningService.createNewElearningContent((long) 4, elearningContent);
+
+        videoService.createNewVideoContent((long) 5, videoContent);
+        videoService.createNewVideoContent((long) 6, videoContent);
+
+        elearningService.getContentCache().printCacheSize();
+        videoService.getContentCache().printCacheSize();
+    }
 }
