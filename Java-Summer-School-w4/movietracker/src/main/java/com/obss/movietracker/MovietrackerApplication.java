b9d3cca530aaace7444d0ee3f1b38f29881b7190
afozbek <furkanozbek1995@gmail.com>
2019-07-26 13:24:57 +0300

    Updating main

--- Java-Summer-School-w4/movietracker/src/main/java/com/obss/movietracker/MovietrackerApplication.java
+++ Java-Summer-School-w4/movietracker/src/main/java/com/obss/movietracker/MovietrackerApplication.java
@@ -1,15 +1,36 @@
 package com.obss.movietracker;
 
+import com.google.common.hash.Hashing;
+import com.obss.movietracker.springwebservice.DAO.UserRepository;
+import com.obss.movietracker.springwebservice.Model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.charset.StandardCharsets;
+
 @SpringBootApplication
-public class MovietrackerApplication {
+public class MovietrackerApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
 
     public static void main(String[] args) {
 
         SpringApplication.run(MovietrackerApplication.class, args);
+
+
         System.out.println("Application is running correctly 😊");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.save(new UserEntity(
+                "test@test.com",
+                Hashing.sha256()
+                        .hashString("test", StandardCharsets.UTF_8)
+                        .toString(), true));
     }
 
 }
