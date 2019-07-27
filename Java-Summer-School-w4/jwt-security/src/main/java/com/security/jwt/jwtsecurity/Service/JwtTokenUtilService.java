ec56974a0394ba750740bd9162bbe70da6798f4b
afozbek <furkanozbek1995@gmail.com>
2019-07-27 21:23:35 +0300

    JwtTokenUtilService added

---
+++ Java-Summer-School-w4/jwt-security/src/main/java/com/security/jwt/jwtsecurity/Service/JwtTokenUtilService.java
@@ -0,0 +1,103 @@
+package com.security.jwt.jwtsecurity.Service;
+
+import com.security.jwt.jwtsecurity.Model.JwtUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtTokenUtilService {
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String getUserNameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getIssuedAtDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getIssuedAt);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    private Boolean ignoreTokenExpiration(String token) {
+        // here you specify tokens, for that the expiration is ignored
+        return false;
+    }
+
+    public String generateToken(JwtUser jwtUser) {
+        String userName = jwtUser.getUserName();
+        Claims claims = Jwts.claims().setSubject(userName);
+
+        claims.put("userId", String.valueOf(jwtUser.getId()));
+        claims.put("role", jwtUser.getRole());
+
+        return doGenerateToken(claims);
+    }
+
+    private String doGenerateToken(Claims claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public Boolean canTokenBeRefreshed(String token) {
+        return (!isTokenExpired(token) || ignoreTokenExpiration(token));
+    }
+
+    public Boolean validateToken(String token, JwtUser jwtUser) {
+        final String username = getUserNameFromToken(token);
+
+        return (username.equals(jwtUser.getUserName()) && !isTokenExpired(token));
+    }
+
+    public JwtUser getJwtUserWithToken(String token) {
+        JwtUser jwtUser = null;
+
+        try {
+            Claims body = Jwts
+                    .parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            jwtUser = new JwtUser();
+
+            jwtUser.setUserName(body.getSubject());
+            jwtUser.setId(Long.parseLong((String) body.get("userId")));
+            jwtUser.setRole((String) body.get("role"));
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        return jwtUser;
+    }
+}
