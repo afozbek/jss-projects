b2da02d80cb28386ac194836701c60ae389dad1b
afozbek <furkanozbek1995@gmail.com>
2019-07-30 16:48:18 +0300

    adding quiz route

--- Java-Summer-School-w5/ReactCompJSS/src/AppRouter.js
+++ Java-Summer-School-w5/ReactCompJSS/src/AppRouter.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Counter from "./Components/CounterApp/Counter";
 import RegisterForm from "./Components/RegisterForm/RegisterForm";
 import Home from "./Components/Home";
+import Quiz from "./Components/Quiz/Quiz";
 
 const AppRouter = props => {
     return (
@@ -11,6 +12,7 @@ const AppRouter = props => {
             <Switch>
                 <Route path="/counter/:count" exact component={Counter} />
                 <Route path="/counter" exact component={Counter} />
+                <Route path="/quiz" exact component={Quiz} />
                 <Route path="/register" exact component={RegisterForm} />
                 <Route path="/" exact component={Home} />
             </Switch>
