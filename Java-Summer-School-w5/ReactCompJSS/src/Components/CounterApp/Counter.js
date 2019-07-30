af67e31c5b14724ae3e26349d036ce0b6d4ece3a
afozbek <furkanozbek1995@gmail.com>
2019-07-30 15:56:58 +0300

    adding Links to Pages

--- Java-Summer-School-w5/ReactCompJSS/src/Components/CounterApp/Counter.js
+++ Java-Summer-School-w5/ReactCompJSS/src/Components/CounterApp/Counter.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router-dom";
 const Counter = props => {
-    const [counter, setCounter] = useState(0);
+    const { count } = props.match.params;
+
+    const [counter, setCounter] = useState(count ? +count : 0);
 
     const counterClickHandler = () => {
-        setCounter(counter + props.increment);
+        setCounter(counter + 1);
     };
 
     return (
         <div>
+            <Link to="/">Home Page</Link>
             <h1>{counter}</h1>
             <button className="button" onClick={counterClickHandler}>
                 Increment Counter
--- Java-Summer-School-w5/ReactCompJSS/src/Components/RegisterForm/RegisterForm.js
+++ Java-Summer-School-w5/ReactCompJSS/src/Components/RegisterForm/RegisterForm.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export default class RegisterForm extends Component {
     state = {
@@ -90,144 +91,171 @@ export default class RegisterForm extends Component {
 
     render() {
         return (
-            <form className="form" onSubmit={this.formSubmitHandler}>
-                <div className="inner-container">
-                    <h1 className="header">Register Page</h1>
-                    <div className="form-input">
-                        <label htmlFor="username" className="form-label">
-                            <span className="form-label-text">Firstname :</span>
-                            <input
-                                onChange={this.inputChangeHandler}
-                                className="form-text form-label-input"
-                                id="firstname"
-                                type="text"
-                                name="username"
-                                required
-                            />
-                        </label>
-                    </div>
-                    <div className="form-input">
-                        <label htmlFor="username" className="form-label">
-                            <span className="form-label-text">Lastname:</span>
-                            <input
-                                onChange={this.inputChangeHandler}
-                                className="form-text form-label-input"
-                                id="lastname"
-                                type="text"
-                                name="username"
-                                required
-                            />
-                        </label>
-                    </div>
-                    <div className="form-input">
-                        <label htmlFor="username" className="form-label">
-                            <span className="form-label-text">Username:</span>
-                            <input
-                                onChange={this.inputChangeHandler}
-                                className="form-text form-label-input"
-                                id="username"
-                                type="text"
-                                name="username"
-                                required
-                            />
-                        </label>
-                    </div>
+            <Fragment>
+                <Link to="/">Home Page</Link>
+                <form className="form" onSubmit={this.formSubmitHandler}>
+                    <div className="inner-container">
+                        <h1 className="header">Register Page</h1>
+                        <div className="form-input">
+                            <label htmlFor="username" className="form-label">
+                                <span className="form-label-text">
+                                    Firstname :
+                                </span>
+                                <input
+                                    onChange={this.inputChangeHandler}
+                                    className="form-text form-label-input"
+                                    id="firstname"
+                                    type="text"
+                                    name="username"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div className="form-input">
+                            <label htmlFor="username" className="form-label">
+                                <span className="form-label-text">
+                                    Lastname:
+                                </span>
+                                <input
+                                    onChange={this.inputChangeHandler}
+                                    className="form-text form-label-input"
+                                    id="lastname"
+                                    type="text"
+                                    name="username"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div className="form-input">
+                            <label htmlFor="username" className="form-label">
+                                <span className="form-label-text">
+                                    Username:
+                                </span>
+                                <input
+                                    onChange={this.inputChangeHandler}
+                                    className="form-text form-label-input"
+                                    id="username"
+                                    type="text"
+                                    name="username"
+                                    required
+                                />
+                            </label>
+                        </div>
 
-                    <div className="form-input">
-                        <label htmlFor="password" className="form-label">
-                            <span className="form-label-text">Password: </span>
-                            <input
-                                onChange={this.inputChangeHandler}
-                                className="form-text"
-                                id="password"
-                                type="password"
-                                name="password"
-                                required
-                            />
-                        </label>
-                    </div>
-                    <div className="form-input">
-                        <label htmlFor="passwordAgain" className="form-label">
-                            <span className="form-label-text">
-                                Password-Ag:
-                            </span>
-                            <input
-                                onChange={this.inputChangeHandler}
-                                className="form-text"
-                                id="passwordAgain"
-                                type="password"
-                                name="passwordAgain"
-                                required
-                            />
-                        </label>
-                    </div>
-                    <div className="form-input">
-                        <label htmlFor="city" className="form-label">
-                            <span className="form-label-text">City: </span>
-                            <select
-                                onChange={this.dropDownChangeHandler}
-                                name="sehir"
+                        <div className="form-input">
+                            <label htmlFor="password" className="form-label">
+                                <span className="form-label-text">
+                                    Password:{" "}
+                                </span>
+                                <input
+                                    onChange={this.inputChangeHandler}
+                                    className="form-text"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div className="form-input">
+                            <label
+                                htmlFor="passwordAgain"
+                                className="form-label"
                             >
-                                <option value="istanbul">Istanbul</option>
-                                <option value="ankara">Ankara</option>
-                                <option value="izmir">Izmir</option>
-                            </select>
-                        </label>
-                    </div>
-
-                    <div className="form-input sex-util">
-                        <label className="form-label">
-                            <span className="form-label-text">Sex: </span>
-                            <span className="sex-wrapper">
-                                <label htmlFor="male" className="inline-label">
-                                    <span className="form-label-radio">
-                                        Male:{" "}
-                                    </span>
-                                    <input
-                                        onChange={this.radioButtonChangeHandler}
-                                        className="form-radio"
-                                        id="male"
-                                        type="radio"
-                                        name="sex"
-                                        value="male"
-                                    />
-                                </label>
-                                <label
-                                    htmlFor="female"
-                                    className="inline-label"
+                                <span className="form-label-text">
+                                    Password-Ag:
+                                </span>
+                                <input
+                                    onChange={this.inputChangeHandler}
+                                    className="form-text"
+                                    id="passwordAgain"
+                                    type="password"
+                                    name="passwordAgain"
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div className="form-input">
+                            <label htmlFor="city" className="form-label">
+                                <span className="form-label-text">City: </span>
+                                <select
+                                    onChange={this.dropDownChangeHandler}
+                                    name="sehir"
                                 >
-                                    <span className="form-label-radio">
-                                        Female:
-                                    </span>
-                                    <input
-                                        onChange={this.radioButtonChangeHandler}
-                                        className="form-radio"
-                                        id="female"
-                                        type="radio"
-                                        value="female"
-                                        name="sex"
-                                    />
-                                </label>
-                            </span>
-                        </label>
-                    </div>
-                    <div className="form-input">
-                        <label htmlFor="confirm" className="form-label">
-                            <span className="form-label-text">Confirm: </span>
-                            <input
-                                onChange={this.confirmChangeHandler}
-                                className="form-text"
-                                id="confirm"
-                                type="checkbox"
-                                name="confirm"
-                            />
-                        </label>
-                    </div>
+                                    <option value="istanbul">Istanbul</option>
+                                    <option value="ankara">Ankara</option>
+                                    <option value="izmir">Izmir</option>
+                                </select>
+                            </label>
+                        </div>
 
-                    <h2>{this.state.message}</h2>
-                    <input className="button" type="submit" value="REGISTER" />
-                </div>
-            </form>
+                        <div className="form-input sex-util">
+                            <label className="form-label">
+                                <span className="form-label-text">Sex: </span>
+                                <span className="sex-wrapper">
+                                    <label
+                                        htmlFor="male"
+                                        className="inline-label"
+                                    >
+                                        <span className="form-label-radio">
+                                            Male:{" "}
+                                        </span>
+                                        <input
+                                            onChange={
+                                                this.radioButtonChangeHandler
+                                            }
+                                            className="form-radio"
+                                            id="male"
+                                            type="radio"
+                                            name="sex"
+                                            value="male"
+                                        />
+                                    </label>
+                                    <label
+                                        htmlFor="female"
+                                        className="inline-label"
+                                    >
+                                        <span className="form-label-radio">
+                                            Female:
+                                        </span>
+                                        <input
+                                            onChange={
+                                                this.radioButtonChangeHandler
+                                            }
+                                            className="form-radio"
+                                            id="female"
+                                            type="radio"
+                                            value="female"
+                                            name="sex"
+                                        />
+                                    </label>
+                                </span>
+                            </label>
+                        </div>
+                        <div className="form-input">
+                            <label htmlFor="confirm" className="form-label">
+                                <span className="form-label-text">
+                                    Confirm:{" "}
+                                </span>
+                                <input
+                                    onChange={this.confirmChangeHandler}
+                                    className="form-text"
+                                    id="confirm"
+                                    type="checkbox"
+                                    name="confirm"
+                                />
+                            </label>
+                        </div>
+
+                        <h2>{this.state.message}</h2>
+                        <input
+                            className="button"
+                            type="submit"
+                            value="REGISTER"
+                        />
+                    </div>
+                </form>
+            </Fragment>
         );
     }
 }
