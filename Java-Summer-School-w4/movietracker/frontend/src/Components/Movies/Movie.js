d2c9be56da820b8e866ebefc51561dba820ec2fb
afozbek <furkanozbek1995@gmail.com>
2019-08-01 21:31:12 +0300

    adding font awesome components

--- Java-Summer-School-w4/movietracker/frontend/src/Components/Movies/Movie.js
+++ Java-Summer-School-w4/movietracker/frontend/src/Components/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Movie = props => {
     const { movieId, name, genreType, rating, director } = props.movieData;
@@ -13,10 +15,14 @@ const Movie = props => {
             <td>{rating}</td>
             <td>{director.name}</td>
             <td>
-                <Link to={`/update-movie/${movieId}`}>UPDATE</Link>
+                <Link to={`/update-movie/${movieId}`}>
+                    <FontAwesomeIcon icon={faEdit} />
+                </Link>
             </td>
             <td>
-                <Link to={`/delete-movie-confirm/${movieId}`}>DELETE</Link>
+                <Link to={`/delete-movie-confirm/${movieId}`}>
+                    <FontAwesomeIcon icon={faTrash} />
+                </Link>
             </td>
         </tr>
     );
--- Java-Summer-School-w4/movietracker/frontend/src/Components/Users/User.js
+++ Java-Summer-School-w4/movietracker/frontend/src/Components/Users/User.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const User = props => {
     const { userId, username, firstName } = props.userData;
@@ -11,10 +13,14 @@ const User = props => {
             <td>{username}</td>
             <td>{firstName}</td>
             <td>
-                <Link to={`/update-user/${userId}`}>UPDATE</Link>
+                <Link to={`/update-user/${userId}`}>
+                    <FontAwesomeIcon icon={faEdit} />
+                </Link>
             </td>
             <td>
-                <Link to={`/delete-user-confirm/${username}`}>DELETE</Link>
+                <Link to={`/delete-user-confirm/${username}`}>
+                    <FontAwesomeIcon icon={faTrash} />
+                </Link>
             </td>
         </tr>
     );
