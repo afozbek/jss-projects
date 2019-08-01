67a8bbf5c2b0e0287e2bb5e144ecdf0db7910559
afozbek <furkanozbek1995@gmail.com>
2019-08-01 07:47:43 +0300

    adding update user

--- Java-Summer-School-w4/movietracker/frontend/src/AppRouter.js
+++ Java-Summer-School-w4/movietracker/frontend/src/AppRouter.js
@@ -7,6 +7,7 @@ import LoginForm from "./Components/Auth/LoginForm/LoginForm";
 import Users from "./Components/Users/Users";
 import Movies from "./Components/Movies/Movies";
 import Directors from "./Components/Directors/Directors";
+import UpdateUser from "./Components/Users/UpdateUser";
 
 const AppRouter = props => {
     return (
@@ -15,6 +16,11 @@ const AppRouter = props => {
                 <Route path="/login" exact component={LoginForm} />
                 <Route path="/register" exact component={RegisterForm} />
                 <Route path="/users" exact component={Users} />
+                <Route
+                    path="/update-user/:username"
+                    exact
+                    component={UpdateUser}
+                />
                 <Route path="/movies" exact component={Movies} />
                 <Route path="/directors" exact component={Directors} />
                 <Route path="/" exact component={Home} />
