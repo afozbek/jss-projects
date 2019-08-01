58ad73d505a36da1fbdd6f8e25c33ab23565e85e
afozbek <furkanozbek1995@gmail.com>
2019-08-01 23:11:54 +0300

    getting FavList

---
+++ Java-Summer-School-w4/movietracker/frontend/src/Components/Users/FavLists/FavList.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash, faStar } from "@fortawesome/free-solid-svg-icons";
+import axios from "../../axios-instance";
+
+class Movie extends Component {
+    state = {
+        userId: "",
+        movieId: "",
+        name: "",
+        genreType: "",
+        rating: "",
+        director: ""
+    };
+
+    favButtonClickHandler = e => {
+        const jwttoken = localStorage.getItem("jwttoken");
+
+        if (!jwttoken) {
+            this.props.history.push("/login");
+        }
+
+        const username = localStorage.getItem("username");
+
+        axios
+            .delete(`/${username}/favList/${this.state.movieId}`, {
+                headers: { Authorization: "Bearer " + jwttoken }
+            })
+            .then(res => {
+                console.log(res.data);
+
+                this.setState({
+                    favList: res.data
+                });
+
+                this.props.history.push("/favlist");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    componentDidMount() {
+        const jwttoken = localStorage.getItem("jwttoken");
+
+        if (!jwttoken) {
+            this.props.history.push("/login");
+        }
+
+        const {
+            movieId,
+            name,
+            genreType,
+            rating,
+            director
+        } = this.props.movieData;
+
+        const username = localStorage.getItem("username");
+
+        axios
+            .get("/admin/user/" + username, {
+                headers: { Authorization: "Bearer " + jwttoken }
+            })
+            .then(res => {
+                console.log(res.data);
+
+                this.setState({
+                    movieId,
+                    name,
+                    genreType,
+                    rating,
+                    director,
+                    userId: res.data.userId
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    render() {
+        const {
+            movieId,
+            name,
+            genreType,
+            rating,
+            director
+        } = this.props.movieData;
+
+        return (
+            <tr>
+                <td>
+                    <span
+                        style={{ cursor: "pointer" }}
+                        onClick={this.favButtonClickHandler}
+                    >
+                        <FontAwesomeIcon icon={faTrash} />
+                    </span>
+                </td>
+                <td>{movieId}</td>
+                <td>{name}</td>
+                <td>{genreType}</td>
+                <td>{rating}</td>
+                <td>{director.name}</td>
+                <td>
+                    <Link to={`/update-movie/${movieId}`}>
+                        <FontAwesomeIcon icon={faEdit} />
+                    </Link>
+                </td>
+                <td>
+                    <Link to={`/delete-movie-confirm/${movieId}`}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </Link>
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default Movie;
