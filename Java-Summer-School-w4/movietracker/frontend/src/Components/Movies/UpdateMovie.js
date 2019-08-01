903dfaf68199e8000a9ff5f1a74dd92fd0e5d6c5
afozbek <furkanozbek1995@gmail.com>
2019-08-01 16:10:17 +0300

    adding loading

--- Java-Summer-School-w4/movietracker/frontend/src/Components/Movies/UpdateMovie.js
+++ Java-Summer-School-w4/movietracker/frontend/src/Components/Movies/UpdateMovie.js
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 import axios from "../../axios-instance";
 import Logout from "../Auth/Logout/Logout";
+import Loading from "../../Util/Loading";
 
 export default class UpdateMovie extends Component {
     state = {
         movieData: {},
+        loading: true,
         message: "",
         directorId: "",
         input: {
@@ -48,11 +50,13 @@ export default class UpdateMovie extends Component {
             .then(res => {
                 this.setState({
                     movieData: res.data,
-                    directorId: res.data.director.directorId
+                    directorId: res.data.director.directorId,
+                    loading: false
                 });
             })
             .catch(err => {
                 console.log(err);
+                this.setState({ loading: false });
             });
     }
 
@@ -86,79 +90,80 @@ export default class UpdateMovie extends Component {
             )
             .then(res => {
                 this.setState({
-                    movieData: res.data
+                    movieData: res.data,
+                    loading: false
                 });
 
                 this.props.history.push("/movies");
             })
             .catch(err => {
                 console.log(err);
+                this.setState({ loading: false });
             });
     };
 
     render() {
+        const form = (
+            <form onSubmit={this.formSubmitHandler}>
+                <div className="inner-container">
+                    <h1 className="header">
+                        Update Movie: {this.state.movieData.name}
+                    </h1>
+                    <div className="form-input">
+                        <label htmlFor="name" className="form-label">
+                            <span className="form-label-text">Name:</span>
+                            <input
+                                onChange={this.inputChangeHandler}
+                                className="form-text form-label-input"
+                                placeholder="Enter Movie Name"
+                                id="name"
+                                type="text"
+                                name="name"
+                                required
+                            />
+                        </label>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="genreType" className="form-label">
+                            <span className="form-label-text">Genre:</span>
+                            <input
+                                onChange={this.inputChangeHandler}
+                                className="form-text form-label-input"
+                                placeholder="Enter Movie Genre Type"
+                                id="genreType"
+                                type="text"
+                                name="genreType"
+                            />
+                        </label>
+                    </div>
+                    <div className="form-input">
+                        <label htmlFor="director" className="form-label">
+                            <span className="form-label-text">Director:</span>
+                            <input
+                                onChange={this.inputChangeHandler}
+                                placeholder="Enter your director name"
+                                className="form-text"
+                                id="director"
+                                type="text"
+                                name="director"
+                                required
+                            />
+                        </label>
+                    </div>
+                    <h3>{this.state.message}</h3>
+                    <input className="button" type="submit" value="UPDATE" />
+                </div>
+            </form>
+        );
+
+        const content = this.state.loading ? <Loading /> : form;
+
         return (
             <Fragment>
                 <Logout {...this.props} />
                 <Link to="/movies">To Movies</Link>
                 <Link to="/">Home Page</Link>
-
-                <form onSubmit={this.formSubmitHandler}>
-                    <div className="inner-container">
-                        <h1 className="header">
-                            Update Movie: {this.state.movieData.name}
-                        </h1>
-                        <div className="form-input">
-                            <label htmlFor="name" className="form-label">
-                                <span className="form-label-text">Name:</span>
-                                <input
-                                    onChange={this.inputChangeHandler}
-                                    className="form-text form-label-input"
-                                    placeholder="Enter Movie Name"
-                                    id="name"
-                                    type="text"
-                                    name="name"
-                                    required
-                                />
-                            </label>
-                        </div>
-                        <div className="form-input">
-                            <label htmlFor="genreType" className="form-label">
-                                <span className="form-label-text">Genre:</span>
-                                <input
-                                    onChange={this.inputChangeHandler}
-                                    className="form-text form-label-input"
-                                    placeholder="Enter Movie Genre Type"
-                                    id="genreType"
-                                    type="text"
-                                    name="genreType"
-                                />
-                            </label>
-                        </div>
-                        <div className="form-input">
-                            <label htmlFor="director" className="form-label">
-                                <span className="form-label-text">
-                                    Director:
-                                </span>
-                                <input
-                                    onChange={this.inputChangeHandler}
-                                    placeholder="Enter your director name"
-                                    className="form-text"
-                                    id="director"
-                                    type="text"
-                                    name="director"
-                                    required
-                                />
-                            </label>
-                        </div>
-                        <h3>{this.state.message}</h3>
-                        <input
-                            className="button"
-                            type="submit"
-                            value="UPDATE"
-                        />
-                    </div>
-                </form>
+                {content}
             </Fragment>
         );
     }
